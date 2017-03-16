61a0996e8e301414e2abebdac6c2b6c0eaf71814
pazhukh <pazhukh@gmail.com>
2017-03-16 19:47:37 +0200

    Update form.js

--- form.js
+++ form.js
@@ -1,25 +1,38 @@
-	$('#myForm').on('submit', function(event){
+$('#myForm').on('submit', function(event){
 		event.preventDefault();
 		var formData = $(this).serialize();
 		console.log(decodeURI(formData));
-		$.ajax({
-			type: 'POST',
-			url: 'http://client.it-kitchen.com.ua/mukha/wp-content/themes/mukha/form.php',
-			data: {'formData':formData},
-			cache:false,
-			response: 'text',
-			success: function(data){
-				console.log('server resp--- '+data);
-				if(data=="done"){
-					$('.formSent').fadeIn(); // показуємо повідомлення якщо форма відправлення
-					$('#registerFitting').hide(); // ховаємо форму після вдалої відправки
-				} else {
-					alert('Sorry, error happend. Please try again or contact us manually!')
-				}
-			},
-			error: function(xhr, status, error)
-			{
-				alert('An error occured.. ' + xhr.responseText + '..' + error );
+
+		//check the validate form for brauser wich not support required
+		var req = $(this).find("[required]");
+		$(req).each(function(){
+			//if form not ok => alert message
+			if ( $(this).val() == '' ){	
+				alert("Обовязково заповніть всі поля");
+				event.preventDefault();
+				return false;
+				//if form ok => send to server
+			} else {
+				$.ajax({
+					type: 'POST',
+					url: 'http://client.it-kitchen.com.ua/mukha/wp-content/themes/mukha/form.php',
+					data: {'formData':formData},
+					cache:false,
+					response: 'text',
+					success: function(data){
+						console.log('server resp--- '+data);
+						if(data=="done"){
+							$('.formSent').fadeIn(); //show success message
+							$('#registerFitting').hide(); //hide form
+						} else {
+							alert('Sorry, error happend. Please try again or contact us manually!');
+						}
+					},
+					error: function(xhr, status, error)
+					{
+						alert('An error occured.. ' + xhr.responseText + '..' + error );
+					}
+				});
 			}
 		});
 	});
