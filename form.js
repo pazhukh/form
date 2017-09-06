1f26b7bc756ab94a8b421abb790bc91ac381baf1
pazhukh <pazhukh@gmail.com>
2017-09-06 17:04:56 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,38 +1,28 @@
-$('#myForm').on('submit', function(event){
-		event.preventDefault();
-		var formData = $(this).serialize();
-		console.log(decodeURI(formData));
+//form
+		$('#form').on('submit', function(event){
+			event.preventDefault();
+			var formData = $(this).serialize();
+			console.log(decodeURI(formData));
 
-		//check the validate form for brauser wich not support required
-		var req = $(this).find("[required]");
-		$(req).each(function(){
-			//if form not ok => alert message
-			if ( $(this).val() == '' ){	
-				alert("Обовязково заповніть всі поля");
-				event.preventDefault();
-				return false;
-				//if form ok => send to server
-			} else {
-				$.ajax({
-					type: 'POST',
-					url: 'http://client.it-kitchen.com.ua/mukha/wp-content/themes/mukha/form.php',
-					data: {'formData':formData},
-					cache:false,
-					response: 'text',
-					success: function(data){
-						console.log('server resp--- '+data);
-						if(data=="done"){
-							$('.formSent').fadeIn(); //show success message
-							$('#registerFitting').hide(); //hide form
-						} else {
-							alert('Sorry, error happend. Please try again or contact us manually!');
+			$.ajax({
+				type: 'POST',
+				url: 'http://client.it-kitchen.com.ua/GMI2/form.php',
+				data: {'formData':formData},
+				cache:false,
+				response: 'text',
+				success: function(data){
+					console.log('server resp--- '+data);
+					if(data=="done"){
+						alert('done');
+								$('.formSent').fadeIn(); //show success message
+								$('#registerFitting').hide(); //hide form
+							} else {
+								alert('Sorry, error happend. Please try again or contact us manually!');
+							}
+						},
+						error: function(xhr, status, error)
+						{
+							alert('An error occured.. ' + xhr.responseText + '..' + error );
 						}
-					},
-					error: function(xhr, status, error)
-					{
-						alert('An error occured.. ' + xhr.responseText + '..' + error );
-					}
-				});
-			}
+					});
 		});
-	});
